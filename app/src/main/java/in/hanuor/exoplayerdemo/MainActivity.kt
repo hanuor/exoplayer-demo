1116d93667e67ba6787efd862b6545d6f3a9988e
hanuor <hanuortech@gmail.com>
2017-11-19 17:43:30 +0530

    Added functionality

--- app/src/main/java/in/hanuor/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/in/hanuor/exoplayerdemo/MainActivity.kt
@@ -1,24 +1,40 @@
 package `in`.hanuor.exoplayerdemo
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.android.exoplayer.ExoPlayer
 import com.google.android.exoplayer.ExoPlayer.Factory.newInstance
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer
+import com.google.android.exoplayer.extractor.ExtractorSampleSource
+import com.google.android.exoplayer.upstream.Allocator
+import com.google.android.exoplayer.upstream.DataSource
+import com.google.android.exoplayer.upstream.DefaultAllocator
+import com.google.android.exoplayer.upstream.DefaultUriDataSource
+import com.google.android.exoplayer.util.Util
 
 class MainActivity : AppCompatActivity() {
-
-    var exoPlayer: ExoPlayer ?= null
     companion object {
         var  BUFFER_SEGMENT_SIZE = 64 * 1024
         var BUFFER_SEGMENT_COUNT = 256
     }
+
+    var exoPlayer: ExoPlayer ?= null
+    var audioUri: Uri?= null
+    var allocator: Allocator ?= null
+    var dataSource: DataSource?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         exoPlayer = newInstance(1)
         setContentView(R.layout.activity_main)
-
-
-
+        audioUri = Uri.parse("android.resource://" + packageName + "/assets/parkly")
+        allocator = DefaultAllocator(BUFFER_SEGMENT_SIZE)
+        var userAgent = Util.getUserAgent(this, "ExoPlayerDemo")
+        dataSource = DefaultUriDataSource(this, null, userAgent)
+        var sampleSource = ExtractorSampleSource(audioUri, dataSource, allocator, BUFFER_SEGMENT_SIZE* BUFFER_SEGMENT_COUNT)
+        var audioRenderer = MediaCodecAudioTrackRenderer(sampleSource)
+        exoPlayer?.prepare(audioRenderer)
+        exoPlayer?.playWhenReady = true
 
     }
 
