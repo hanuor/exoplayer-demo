d1b48804aea55d72a984ed3b0ffbf7183039c406
hanuor <hanuortech@gmail.com>
2017-11-19 17:24:04 +0530

    Created a new instance

--- app/src/main/java/in/hanuor/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/in/hanuor/exoplayerdemo/MainActivity.kt
@@ -2,13 +2,22 @@ package `in`.hanuor.exoplayerdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.exoplayer.ExoPlayer
+import com.google.android.exoplayer.ExoPlayer.Factory.newInstance
 
 class MainActivity : AppCompatActivity() {
 
+    var exoPlayer: ExoPlayer ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        exoPlayer = newInstance(1)
+
         setContentView(R.layout.activity_main)
+
+
+
     }
 
-    
+
 }
